71ed7214c3c77033983b45ab43ade24182f98d6e
Douglas Neiva <douglasneiva.60@gmail.com>
2023-05-02 21:48:51 -0300

    Added "greet" function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>  // Standard C++ header file to print text
 
-int main()
+void greet()
 {
 	// Print some welcome text 
 	std::cout << "Welcome!\n\n";
 	// Print "I'm alive" message
 	std::cout << "Hello World! \n\n";
+}
+
+int main()
+{
+	greet();
+
 }
 
